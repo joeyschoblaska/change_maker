cb1f16ac4a70ec529bfebd75d9ae3002ace79378
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-08-24 13:44:13 -0500

    initial commit

---
+++ change.rb
@@ -0,0 +1,4 @@
+class Change
+  def self.make(amount, coins = [25, 10, 5, 1])
+  end
+end
---
+++ change_spec.rb
@@ -0,0 +1,7 @@
+require "./change"
+
+describe Change do
+  it "example spec" do
+    expect(1).to eq(1)
+  end
+end
